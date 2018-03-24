8b2989db2689ce3e1497f93b6f46af3e82d2599b
Microsoft Open Source <microsoftopensource@users.noreply.github.com>
2018-03-24 14:05:24 -0700

    Create sample app

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import { WelcomeScreen } from './screens/welcomeScreen';
+import { BuildScreen } from './screens/buildScreen';
+import { TestScreen } from './screens/testScreen';
+import { DistributeScreen } from './screens/distributeScreen';
+import { CrashesScreen } from './screens/crashesScreen';
+import { AnalyticsScreen } from './screens/analyticsScreen';
+import { PushScreen } from './screens/pushScreen';
+import { CodePushScreen } from './screens/codePushScreen';
+
+const RootTabNavigator = TabNavigator(
+  {
+    Welcome: {
+      screen: WelcomeScreen,
+    },
+    Build: {
+      screen: BuildScreen,
+    },
+    Test: {
+      screen: TestScreen,
+    },
+    Distribute: {
+      screen: DistributeScreen,
+    },
+    Crashes: {
+      screen: CrashesScreen,
+    },
+    Analytics: {
+      screen: AnalyticsScreen,
+    },
+    Push: {
+      screen: PushScreen,
+    },
+    CodePush: {
+      screen: CodePushScreen
+    }
+  },
+  {
+    initialRouteName: 'Welcome',
+    tabBarOptions: {
+      style: {
+        backgroundColor: "#252525"
+      },
+    },
+    navigationOptions: {
+      tabBarVisible: false,
+    },
+    lazy: false,
+    swipeEnabled: true
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootTabNavigator />;
+  }
+}
---
+++ src/screens/distributeScreen.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { View, Text, Image } from 'react-native';
+
+import BaseScreen from '../components/baseScreen';
+import images from '../images';
+
+export class DistributeScreen extends React.Component {
+    render() {
+        return (
+            <BaseScreen options={
+                {
+                    title: "Distribute",
+                    topContainer: {
+                        height: 200,
+                        backgroundColor: "#38A495",
+                        imageSource: images.distribute
+                    },
+                    bottomContainer: {
+                        backgroundColor: "#44B8A8",
+                        description: "Users can install the app via email distribution lists for testing, much as they\'d download an app from the app store.\n\nBacked by HockeyApp."
+                    }
+                }
+            }>
+            </BaseScreen>
+        );
+    }
+}
