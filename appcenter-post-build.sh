d846a721b52c77eb114119aa8b13b1f19934a533
Patrick Nikoletich <pnikoletich@gmail.com>
2018-05-05 21:50:06 -0700

    Update post-build script.

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,5 +1,5 @@
 # Start async test run and fetch the test run
-TEST_RUN=$(appcenter-cli test run xcuitest --app "Build2018/ac-github-ios" --devices "Build2018/release-devices" --test-series master --locale en_US --build-dir DerivedData/Build/Products/Release-iphoneos --token  --async | pcregrep -o1 -M 'Test run id: "(.+)"');
+TEST_RUN=$(appcenter test run xcuitest --app "Build2018/ac-github-ios" --devices "Build2018/release" --test-series master --locale en_US --build-dir DerivedData/Build/Products/Release-iphoneos --token $APP_CENTER_TOKEN --async | pcregrep -o1 -M 'Test run id: "(.+)"');
 echo "Started new test run: $TEST_RUN"
 
 # Trigger Logic App to monitor test run
