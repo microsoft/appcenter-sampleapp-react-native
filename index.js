ef52a7d881a87a3dc16f8728431e7556763a42b9
histur <v-histur@microsoft.com>
2019-11-27 13:07:04 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/**
+ * @format
+ */
+
 import './src/index';
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+AppRegistry.registerComponent(appName, () => App);
