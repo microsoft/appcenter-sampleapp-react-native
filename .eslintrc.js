aa9fa3f2cb9cf7b0783aee59651c8a012d927c92
Vyas Nellutla <vyasknellutla@gmail.com>
2019-12-19 15:30:15 -0600

    setup prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: [
-    "@typescript-eslint",
+    "@typescript-eslint"
     // any other eslint plugins your team might need/use
   ],
   env: {
@@ -17,18 +17,22 @@ module.exports = {
     project: [
       // include all of your `tsconfig.json` files in here
       // the example here is taken from portal
-      path.resolve(__dirname, './tsconfig.json'), 
-      path.resolve(__dirname, './server/tsconfig.json'),
-      path.resolve(__dirname, './fixtures/tsconfig.json'),
+      path.resolve(__dirname, "./tsconfig.json"),
+      path.resolve(__dirname, "./server/tsconfig.json"),
+      path.resolve(__dirname, "./fixtures/tsconfig.json")
     ],
     tsconfigRootDir: __dirname,
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: "module"
   },
   // for getting help/started on picking rules, a good place to look for
   // a minimal setup that includes only a couple stylistic rules is
   // in the portal's eslintrc.js file
   rules: {
     // ...
-  }
-}
+  },
+  extends: [
+    // any other plugins here,
+    "plugin:prettier/recommended"
+  ]
+};
--- android/app/src/main/java/com/appcentersample/MainApplication.java
+++ android/app/src/main/java/com/appcentersample/MainApplication.java
@@ -1,6 +1,7 @@
 package com.appcentersample;
 
 import android.app.Application;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
