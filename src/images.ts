6a17e050c8d4d276c4163a2d88e1f939c8ace497
AnatolyPristensky <84129877+AnatolyPristensky@users.noreply.github.com>
2021-08-11 14:53:58 +0300

    Removed unused assets

--- src/images.ts
+++ src/images.ts
@@ -4,7 +4,6 @@ export default {
   build: require("../res/build.png"),
   crashes: require("../res/crashes.png"),
   distribute: require("../res/distribute.png"),
-  push: require("../res/push.png"),
   test: require("../res/test.png"),
   codepush: require("../res/codepush.png")
 };
