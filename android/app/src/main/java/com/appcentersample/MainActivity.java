7a78cdf8a040d8515ed075854d7dad1905fce7c8
Dmitriy Kirakosyan <v-dmkira@microsoft.com>
2022-07-04 13:20:55 +0700

    Update libs
    Perform updates requiring for react-native 0.69.
    Also, as a workaround use prebuilt appcenter-sdk packages with the fix of
    https://github.com/microsoft/appcenter-sdk-react-native/issues/979.

--- android/app/src/main/java/com/appcentersample/MainActivity.java
+++ android/app/src/main/java/com/appcentersample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.appcentersample;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -7,6 +8,10 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
+    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
