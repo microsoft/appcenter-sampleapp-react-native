0415364dbacd4cf35c99e4a28a12ef9f3211c04d
Dmitriy Kirakosyan <v-dmkira@microsoft.com>
2022-07-07 11:15:00 +0700

    Fix native module linking

--- android/app/src/main/java/com/appcentersample/MainApplication.java
+++ android/app/src/main/java/com/appcentersample/MainApplication.java
@@ -26,8 +26,10 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
+            @SuppressWarnings("UnnecessaryLocalVariable")
             List<ReactPackage> packages = new PackageList(this).getPackages();
-            
+
+            packages.add(new AppCenterSamplePackage());
             return packages;
         }
 
